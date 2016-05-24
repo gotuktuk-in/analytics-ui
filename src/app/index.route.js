0bd6141f86c4cf367c100a36058ff2f142c3059e
Pavan Shinde <pavan.shinde@47billlion.com>
2016-05-24 12:26:07 +0530

    changes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -22,7 +22,7 @@
             abstract:true
       })
      .state('home.analytics', {
-        url: 'analytics',
+        url: 'analytics/:city/:vehicleType',
         templateUrl: 'app/analytics/analytics.html',
         controller: 'AnalyticsController',
         controllerAs: 'vm'
@@ -40,7 +40,7 @@
           controllerAs: 'drivers'
       });
 
-    $urlRouterProvider.otherwise('/analytics');
+    $urlRouterProvider.otherwise('/login');
   }
 
 })();
