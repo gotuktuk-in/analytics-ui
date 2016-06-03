022586a3f2bdcc40e36607e54815e2ea55337208
Pavan Shinde <pavan.shinde@47billlion.com>
2016-06-03 16:26:17 +0530

    frequency selection added in trips

--- src/app/trips/trips.controller.js
+++ src/app/trips/trips.controller.js
@@ -22,6 +22,24 @@
         vm.tripChartOptions = angular.copy(ChartConfigService.lineChartOptions);
 
         vm.trips = [];
+
+        this.changeFrequency = function (section, freqModel) {
+            console.log("Frequency changed ", freqModel.value)
+
+            vm.tripChartOptions.chart.xAxis.axisLabel = freqModel
+            if (freqModel == 'hour') {
+                vm.tripChartOptions.chart.xAxis.tickFormat = function (d) {
+                    return d3.time.format('%d %b %I %p')(new Date(d));
+                };
+            }
+            else {
+                vm.tripChartOptions.chart.xAxis.tickFormat = function (d) {
+                    return d3.time.format('%d %b %y')(new Date(d));
+                };
+            }
+            vm.getTrips()
+           
+        }
         vm.getTrips = function () {
             PerformanceService.getTrips({
                 city: $rootScope.city,
@@ -41,19 +59,20 @@
             $scope.tableParams.reload();
         }
         var sorting;
-        function setDate()
-        {
+
+        function setDate() {
 
         }
-        $scope.tableParams = new NgTableParams({page:1,count: 10}, {
+
+        $scope.tableParams = new NgTableParams({page: 1, count: 10}, {
             counts: [],
             getData: function (params) {
                 // ajax request to api
-                var start = ((params.page() - 1) * 10)+1;
+                var start = ((params.page() - 1) * 10) + 1;
                 console.log("**************************")
                 var startDate = moment($scope.tripDates.startDate).unix()
                 var endDate = moment($scope.tripDates.endDate).unix()
-                var dataObj ={}
+                var dataObj = {}
                 dataObj.start = start;
                 dataObj.count = params.count();
                 dataObj.startDate = moment($scope.tripDates.startDate).unix();
@@ -68,16 +87,16 @@
                     else {
                         dataObj.orderby = "DESC"
                     }
-                  }
+                }
                 return TripsService.getAllTrips(dataObj).$promise.then(function (data) {
 
                     params.total(data.total); // recal. page nav controls
 
-                    console.log('trop ',data)
-                    if(data.data.length > 0){
+                    console.log('trop ', data)
+                    if (data.data.length > 0) {
                         $scope.tblNoData = false
                     }
-                    else{
+                    else {
                         $scope.tblNoData = true
                     }
                     return data.data;
@@ -88,13 +107,13 @@
         heatmapChart(trips_heatmap);
     }
 })();
-var margin = { top: 50, right: 0, bottom: 100, left: 30 },
+var margin = {top: 50, right: 0, bottom: 100, left: 30},
     width = 960 - margin.left - margin.right,
     height = 430 - margin.top - margin.bottom,
     gridSize = Math.floor(width / 24),
-    legendElementWidth = gridSize*2,
+    legendElementWidth = gridSize * 2,
     buckets = 9,
-    colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+    colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"], // alternatively colorbrewer.YlGnBu[9]
     days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
     times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
 datasets = ["data.tsv", "data2.tsv"];
@@ -108,45 +127,65 @@ var svg = d3.select("#chart").append("svg")
 var dayLabels = svg.selectAll(".dayLabel")
     .data(days)
     .enter().append("text")
-    .text(function (d) { return d; })
+    .text(function (d) {
+        return d;
+    })
     .attr("x", 0)
-    .attr("y", function (d, i) { return i * gridSize; })
+    .attr("y", function (d, i) {
+        return i * gridSize;
+    })
     .style("text-anchor", "end")
     .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-    .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+    .attr("class", function (d, i) {
+        return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis");
+    });
 
 var timeLabels = svg.selectAll(".timeLabel")
     .data(times)
     .enter().append("text")
-    .text(function(d) { return d; })
-    .attr("x", function(d, i) { return i * gridSize; })
+    .text(function (d) {
+        return d;
+    })
+    .attr("x", function (d, i) {
+        return i * gridSize;
+    })
     .attr("y", 0)
     .style("text-anchor", "middle")
     .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-    .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+    .attr("class", function (d, i) {
+        return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
+    });
 
-var heatmapChart = function(tsvFile) {
+var heatmapChart = function (tsvFile) {
     d3.tsv(tsvFile,
-        function(d) {
+        function (d) {
             return {
                 day: +d.day,
                 hour: +d.hour,
                 value: +d.value
             };
         },
-        function(error, data) {
+        function (error, data) {
             var colorScale = d3.scale.quantile()
-                .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
+                .domain([0, buckets - 1, d3.max(data, function (d) {
+                    return d.value;
+                })])
                 .range(colors);
 
             var cards = svg.selectAll(".hour")
-                .data(data, function(d) {return d.day+':'+d.hour;});
+                .data(data, function (d) {
+                    return d.day + ':' + d.hour;
+                });
 
             cards.append("title");
 
             cards.enter().append("rect")
-                .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-                .attr("y", function(d) { return (d.day - 1) * gridSize; })
+                .attr("x", function (d) {
+                    return (d.hour - 1) * gridSize;
+                })
+                .attr("y", function (d) {
+                    return (d.day - 1) * gridSize;
+                })
                 .attr("rx", 4)
                 .attr("ry", 4)
                 .attr("class", "hour bordered")
@@ -155,29 +194,43 @@ var heatmapChart = function(tsvFile) {
                 .style("fill", colors[0]);
 
             cards.transition().duration(1000)
-                .style("fill", function(d) { return colorScale(d.value); });
+                .style("fill", function (d) {
+                    return colorScale(d.value);
+                });
 
-            cards.select("title").text(function(d) { return d.value; });
+            cards.select("title").text(function (d) {
+                return d.value;
+            });
 
             cards.exit().remove();
 
             var legend = svg.selectAll(".legend")
-                .data([0].concat(colorScale.quantiles()), function(d) { return d; });
+                .data([0].concat(colorScale.quantiles()), function (d) {
+                    return d;
+                });
 
             legend.enter().append("g")
                 .attr("class", "legend");
 
             legend.append("rect")
-                .attr("x", function(d, i) { return legendElementWidth * i; })
+                .attr("x", function (d, i) {
+                    return legendElementWidth * i;
+                })
                 .attr("y", height)
                 .attr("width", legendElementWidth)
                 .attr("height", gridSize / 2)
-                .style("fill", function(d, i) { return colors[i]; });
+                .style("fill", function (d, i) {
+                    return colors[i];
+                });
 
             legend.append("text")
                 .attr("class", "mono")
-                .text(function(d) { return "? " + Math.round(d); })
-                .attr("x", function(d, i) { return legendElementWidth * i; })
+                .text(function (d) {
+                    return "? " + Math.round(d);
+                })
+                .attr("x", function (d, i) {
+                    return legendElementWidth * i;
+                })
                 .attr("y", height + gridSize);
 
             legend.exit().remove();
