4f81ce2d125cdfa37f1cf85cc7260afe8a3d3d25
Ashish Sahu <ashishsahu13@gmail.com>
2016-07-12 18:14:45 +0530

    updated files

--- src/app/surge/surge.controller.js
+++ src/app/surge/surge.controller.js
@@ -3,10 +3,10 @@
 
     angular
         .module('tuktukV2Dahboard')
-        .controller('surgeController', GeoMapController);
+        .controller('surgeController', surgeController);
 
     /** @ngInject */
-    function GeoMapController($scope, $log, $rootScope, $interval, StaticDataService, ChartConfigService, PerformanceService, PerformanceHandler, RidersService, NgTableParams, ngTableEventsChannel, LiveService, $resource) {
+    function surgeController($scope, $log, $rootScope, $interval, StaticDataService, PerformanceService, PerformanceHandler, RidersService, NgTableParams, ngTableEventsChannel, LiveService, $resource) {
 
         var vm = this;
         var map;
