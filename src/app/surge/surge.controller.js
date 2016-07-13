6fbb5fe45ced60eec41b4b9f80d686d2e0be24c6
Ashish Sahu <ashishsahu13@gmail.com>
2016-07-13 11:59:14 +0530

    date issue

--- src/app/surge/surge.controller.js
+++ src/app/surge/surge.controller.js
@@ -6,16 +6,41 @@
         .controller('surgeController', surgeController);
 
     /** @ngInject */
-    function surgeController($scope, $log, $rootScope, $interval, StaticDataService, PerformanceService, PerformanceHandler, RidersService, NgTableParams, ngTableEventsChannel, LiveService, $resource) {
+    function surgeController($scope, $log, $rootScope, MapService, NgMap, AUTH_API) {
 
+        $scope.toggle = false;
         var vm = this;
         var map;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('tuktukGeoMap'), {
-                center: {lat: -34.397, lng: 150.644},
-                zoom: 8
+        NgMap.getMap({id:'drivers_map'}).then(function (map) {
+            vm.map = map;
+            if(vm.map && vm.map.data)
+            {
+                vm.map.data.forEach(function(feature) {
+                    //filter...
+                    vm.map.data.remove(feature);
+                });
+            }
+            map.data.setStyle({
+                fillColor: 'gray',
+                strokeWeight: 1,
+                strokeOpacity:.1,
+                fillOpacity:.1
             });
-        }
-        initMap()
+            MapService.loadGeoJson({}, function (response) {
+                    //vm.geoJSON = response
+                    /*  var temp = {}
+                     temp.features = response.features
+                     temp.type = response.type
+                     vm.geoJSON=JSON.parse(angular.toJson(temp))
+                     console.log("typeof vm.geoJSON", typeof  vm.geoJSON);*/
+                    vm.geoJSON = {}
+                    vm.geoJSON = response
+                    vm.map.data.addGeoJson(vm.geoJSON );
+
+                }, function (error) {
+                    console.log("error ", error)
+                }
+            )
+        });
     }
 })();
