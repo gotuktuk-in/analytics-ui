1efb2558e05c602f91ecfdc7d416af39d5e81353
Ashish Sahu <ashishsahu13@gmail.com>
2016-07-22 21:42:57 +0530

    update alert surge

--- src/app/surge/surge.controller.js
+++ src/app/surge/surge.controller.js
@@ -14,6 +14,7 @@
         vm.geohashGroups = []
         vm.setting = {};
         vm.colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+        //vm.colors = ["#20b2aa", "#fa8072", "#87ceeb", "#daa520", "#00bfff", "#dc143c", "#87cefa", "#90ee90", "#add8e6", "#d3d3d3"];
         vm.hstep = 1;
         vm.mstep = 15;
         $scope.ismeridian = false;
@@ -25,9 +26,11 @@
             value: 7
         }, {name: "8", value: 8}]
         initSetting()
-        vm.selectedPrecision = vm.precisionArr[2];
+        vm.selectedPrecision = vm.precisionArr[3];
         function initSetting()
         {
+            vm.setting.value=''
+            vm.setting.driverValue=''
             vm.setting.groupTitle = ''
             vm.setting.type = 'multiply'
             vm.setting.geoHashArr = []
@@ -98,6 +101,8 @@
         }
         vm.removeGroup = function (id) {
             SurgeService.removeGroup({id: id}, function (response) {
+                initSetting()
+                getAllGroups()
                 toastr.success(response);
             }, function (error) {
                 toastr.success(error);
