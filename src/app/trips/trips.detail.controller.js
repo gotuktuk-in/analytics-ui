68d8aa82318a0a4aab32e8b5e3cd0bdccfd3e27c
sumitpatrikar <sumit.patrikar@47billion.com>
2016-08-06 04:20:27 +0530

    WIP for tooltip in map

--- src/app/trips/trips.detail.controller.js
+++ src/app/trips/trips.detail.controller.js
@@ -16,22 +16,90 @@
 
         $scope.selectedTrip;
         $scope.selectedTripBid;
+        vm.bidDr;
+        vm.drID;
+        vm.drID2;
+        vm.curDrID;
+        vm.curDrPh;
+        vm.curDrVhNo;
+        vm.curDrJD;
+        vm.curDrURide;
+        vm.curDrTRide;
+        vm.curDrToURide;
+        vm.curDrToRide;
+        vm.curDriDisFT;
+        vm.curDriDisIT;
+        vm.curDriDisTo;
+        vm.curDriOnHrFT;
+        vm.curDriOnHrIT;
+        vm.curDriOnHrTo;
+        vm.curDriFeed;
+        vm.curDriTofare;
+        vm.curDriEar;
+        vm.curDriToEar;
 
-        //function getBid() {
-        //    TripsService.getBidDetail(
-        //        {id: $stateParams.id}, function (data) {
-        //            $scope.data = data;
-        //        }, function (err) {
-        //            console.log(err)
-        //            $scope.error = true;
-        //        });
-        //}
+        vm.getBid = function() {
+            console.log('sfdsd');
+           TripsService.getBidDetail(
+                {
+                    id: $stateParams.id
+                }, function (response) {
+                    $scope.selectedTripBid = response;
+                    vm.bidDr = $scope.selectedTripBid;
+                    //vm.drID = $scope.selectedTripBid[i].dr;
+                    console.log('selectedTripBid-dr', $scope.selectedTripBid);
+                    $scope.totalBid = response.length;
+                    vm.initialize()
+                }, function (err) {
+                    console.log(err)
+                    $scope.error = true;
+                });
+        }
         //
         //getBid();
+        vm.getDriverDetail = function(){
+            var z = vm.drID;
+            vm.drID2 = vm.bidDr[z].dr;
+            TripsService.getProfile(
+                {
+                    id:  vm.drID2,
+                    from: moment($scope.selectedDates.startDate).startOf('day').format("YYYYMMDD").toString(),
+                    to: moment($scope.selectedDates.endDate).endOf('day').format("YYYYMMDD").toString()
+                }, function (response) {
+                    console.log('uu', response);
+                    vm.profile = response;
+                    vm.curDrID = vm.profile.id ,
+                    vm.curDrPh = vm.profile.phone ,
+                    vm.curDrVhNo = vm.profile.vehicle ,
+                    vm.curDrJD = vm.profile.joinedOn ,
+                    vm.curDrURide = vm.profile.rides_today.unique_ride ,
+                    vm.curDrTRide = vm.profile.rides_today.total ,
+                    vm.curDrToURide = vm.profile.rides_total.unique_ride ,
+                    vm.curDrToRide = vm.profile.rides_total.total ,
+                    vm.curDriDisFT = vm.profile.today_distance.forTrip ,
+                    vm.curDriDisIT = vm.profile.today_distance.intrip ,
+                    vm.curDriDisTo = vm.profile.today_distance.total ,
+                    vm.curDriOnHrFT = vm.profile.today_online.fortrip_time ,
+                    vm.curDriOnHrIT = vm.profile.today_online.intrip_time ,
+                    vm.curDriOnHrTo = vm.profile.today_online.total_hours ,
+                    vm.curDriFeed = vm.profile.feedbackRating ,
+                    vm.curDriTofare = vm.profile.total_fare ,
+                    vm.curDriEar = vm.profile.today_earning ,
+                    vm.curDriToEar = vm.profile.total_earning ;
+                    
+
+                }, function (err) {
+                    console.log(err)
+                    $scope.error = true;
+                }
+            );
+        }
+
+        
 
         function getDetails() {
 
-            TripsService.getBidDetail(
+            /*TripsService.getBidDetail(
                 {
                     id: $stateParams.id
                 }, function (response) {
@@ -40,19 +108,21 @@
                 }, function (err) {
                     console.log(err)
                     $scope.error = true;
-                });
+                });*/
 
-            TripsService.getProfile(
+            /*TripsService.getProfile(
+                console.log('uu', $stateParams.driverId);
                 {
                     id: $stateParams.driverId,
                     from: moment($scope.selectedDates.startDate).startOf('day').format("YYYYMMDD").toString(),
                     to: moment($scope.selectedDates.endDate).endOf('day').format("YYYYMMDD").toString()
                 }, function (response) {
                     vm.profile = response;
+                    vm.initialize()
                 }, function (err) {
                     console.log(err)
                     $scope.error = true;
-                });
+                });*/
 
 
             TripsService.getTripDetail(
@@ -62,14 +132,14 @@
                     $scope.selectedTrip = response;
                     vm.snapCodesFor = $scope.selectedTrip.forTripSnapCode;
                     vm.snapCodesIn = $scope.selectedTrip.inTripSnapCode;
-                    initialize();
+                    vm.initialize();
                 }, function (err) {
                     console.log(err)
                     $scope.error = true;
                 });
 
 
-            function initialize() {
+            vm.initialize = function() {
                 var encodedStringFor = vm.snapCodesFor;
                 var encodedStringIn = vm.snapCodesIn;
 
@@ -110,7 +180,6 @@
                 var icon1 = {
                     url: "assets/images/icons/marker-green.svg", // url
                     scaledSize: new google.maps.Size(30, 30) // scaled size
-
                 };
                 var icon2 = {
                     url: "assets/images/icons/marker-red.svg", // url
@@ -131,18 +200,21 @@
 
 
                 for (i = 0; i < $scope.totalBid; i++) {
-
+                    
                     markers[i] = new google.maps.Marker({
                         position: new google.maps.LatLng($scope.selectedTripBid[i].lt, $scope.selectedTripBid[i].ln),
                         map: map,
-                        title: 'Bid',
+                        title: 'Bid' + [i],
                         icon: icon3
                     });
                     markers[i].index = i;
-                    contents[i] = '<div class="popup_container">'
-                    + $scope.selectedTripBid[i].lt
-                    + ','
-                    + $scope.selectedTripBid[i].ln
+                    
+                    contents[i] = '<div class="popup_container">' + 
+                    '<div class="col-md-6 col-sm-4 col-xs-12"><h4 class="number ng-binding">' + $scope.selectedTripBid[i].lt +  '</h4><span>Lt</span></div>'
+                    + '<div class="col-md-6 col-sm-4 col-xs-12"><h4 class="number ng-binding">' + $scope.selectedTripBid[i].ln + '</h4><span>Lng</span></div>'
+                    + '<div class="col-md-6 col-sm-4 col-xs-12"><h4 class="number ng-binding">' + vm.curDrID + '</h4><span>Id</span></div>'
+                    + '<div class="col-md-6 col-sm-4 col-xs-12"><h4 class="number ng-binding">' + vm.curDrPh + '</h4><span>Id</span></div>'
+                    + 
                     '</div>';
 
 
@@ -152,8 +224,12 @@
                     });
 
                     google.maps.event.addListener(markers[i], 'click', function () {
-                        console.log(this.index); // this will give correct index
-                        console.log(i); //this will always give 10 for you
+                        //console.log(this.index); // this will give correct index
+                        //console.log(i); //this will always give 10 for you
+                        vm.drID = this.index;
+                        console.log(vm.drID);
+                        vm.getDriverDetail(vm.drID)
+
                         infowindows[this.index].open(map, markers[this.index]);
                         map.panTo(markers[this.index].getPosition());
                     });
@@ -220,10 +296,7 @@
                         icon: icon2,
                         map: map
                     });
-
                 }
-
-
             }
 
             //function decodeLevels(encodedLevelsString) {
