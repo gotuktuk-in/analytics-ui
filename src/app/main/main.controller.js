02c42a1a379ffb88d01d4a71e240f8a01ead7f22
sumitpatrikar <sumit.patrikar@47billion.com>
2016-08-09 18:21:36 +0530

    secods to minute second conversion

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -48,3 +48,12 @@
 
   }
 })();
+angular
+    .module('tuktukV2Dahboard').filter('toMinSec', function(){
+  return function(input){
+    var minutes = parseInt(input/60, 10);
+    var seconds = input%60;
+    
+    return minutes+' Min.'+(seconds ? ' & '+seconds+' Sec.' : '');
+  }
+})
