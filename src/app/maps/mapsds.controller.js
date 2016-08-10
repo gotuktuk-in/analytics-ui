00fbbc9d502b489f3f4690c9c4767983d99eb183
Ashish Sahu <ashishsahu13@gmail.com>
2016-08-10 18:56:19 +0530

    updated dist

--- src/app/maps/mapsds.controller.js
+++ src/app/maps/mapsds.controller.js
@@ -9,6 +9,7 @@
         var vm = this;
         vm.map;
         var Geohash = {};
+        $scope.date = moment().format("dddd, MMMM Do YYYY");
         vm.colors = ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'];
         vm.precisionArr = [
             {name: "6", value: 6},
@@ -20,7 +21,6 @@
         NgMap.getMap({id: 'bidMap'}).then(function (map) {
             vm.map = map;
             google.maps.event.addDomListener(vm.map);
-
         });
 
         vm.onPrecisionChange = function () {
@@ -31,6 +31,17 @@
             MapService.getDemandSupply({precision: vm.selectedPrecision.value}, function (response) {
                 $scope.allHash = response;
                 $scope.totalHash = response.length;
+
+                var dateString = $scope.allHash[0].id;
+                var year = dateString.substring(0, 4);
+                var month = dateString.substring(4, 6);
+                var day = dateString.substring(6, 8);
+                var hour = dateString.substring(8, 10);
+                var minutes = dateString.substring(10, 11);
+
+                var newDate = new Date(year, month - 1, day, hour, (minutes - 1) * 15);
+                $scope.dsDate = moment(newDate).format("dddd, MMMM Do YYYY,  h:mm");
+
                 var i = 0
                 _.each($scope.allHash, function (group) {
                     var newObj = group;
