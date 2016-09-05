9696dfff0fff35f49220a59d8f9bd4af91789048
Ashish Sahu <ashishsahu13@gmail.com>
2016-09-05 21:47:00 +0530

    onboarding ui

--- src/app/drivers/onboarding.controller.js
+++ src/app/drivers/onboarding.controller.js
@@ -8,6 +8,15 @@
     /** @ngInject */
     function OnboardingController($scope, $log, $rootScope, $interval, StaticDataService, DriversService, $resource, toastr, ngDialog) {
         var vm = this;
+
+        vm.renderOnboardingForm = function () {
+            vm.ownershipArr = [
+                {name: "Self Owned", value: 0},
+                {name: "Rented", value: 1}
+            ];
+            vm.selectedOwnership = vm.ownershipArr[0];
+        };
+        vm.renderOnboardingForm();
     }
 
 })();
