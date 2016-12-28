1cc27ddbc8c54da230a01bf9f0079e40670e58c0
Ashish Sahu <ashishsahu13@gmail.com>
2016-12-28 15:19:28 +0530

    wallet changes trips page

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,14 +1,14 @@
 /* global malarkey:false, moment:false */
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('tuktukV2Dahboard')
-    .constant('malarkey', malarkey)
-    .constant('moment', moment)
+    angular
+        .module('tuktukV2Dahboard')
+        .constant('malarkey', malarkey)
+        .constant('moment', moment)
 
-      	//.constant('API', "https://analytics.gotuktuk.in/rest/v1/")
-		//.constant('AUTH_API', "https://analytics.gotuktuk.in/rest/o/v1/")
+        //.constant('API', "https://analytics.gotuktuk.in/rest/v1/")
+        //.constant('AUTH_API', "https://analytics.gotuktuk.in/rest/o/v1/")
         .constant('API', "https://dev-api-analytics.gotuktuk.in/rest/v1/")
         .constant('AUTH_API', "https://dev-api-analytics.gotuktuk.in/rest/o/v1/")
 
